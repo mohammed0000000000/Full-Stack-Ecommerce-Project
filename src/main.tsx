83c236cfb830ab7b84259a23e790667aee1c9e77
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-07-23 20:33:47 +0300

    setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import theme from "./utils/theme.ts";
 import { ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
-    <Router>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
-      </ChakraProvider>
-    </Router>
+    {" "}
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+        </ChakraProvider>
+      </Router>
+    </QueryClientProvider>
   </>
 );
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,59 +1,16 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 import ProductCard from "../components/ProductCard";
+import axiosInstance from "../config/axiosConfig";
+import { useQuery } from "@tanstack/react-query";
 
-interface IProps {}
+const ProductsPage = () => {
+  // const getProductsList = async () => {
+  //   const { data} = axiosInstance.get(`${import.meta.env.STRAPI_SERVER_URL}`)
+  // }
+  // const { data, isLoading, error } = useQuery({ queryKey:["products"], queryFn:});
 
-const ProductsPage = ({}: IProps) => {
   return (
     <Grid templateColumns="repeat(auto-fill, minmax(300px,1fr))" gap={4}>
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
-      <ProductCard
-        title="product 1"
-        price={450}
-        description="Lorem"
-        imageURL="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      />
       <ProductCard
         title="product 1"
         price={450}
