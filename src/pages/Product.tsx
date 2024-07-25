e0ed86f591f8348a484ee4e8f747411ca370bdfe
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-07-26 00:02:42 +0300

    Back functionality product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,11 +1,23 @@
-import ProductCard from "../components/ProductCard";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import { IProduct, IProductResponse } from "../interfaces";
+import { IProductResponse } from "../interfaces";
 import ProductSkeleton from "../components/skeleteon/ProductSkeleton";
 import { ReactNode, useEffect } from "react";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Stack,
+  Image,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
+import { ArrowLeftIcon } from "@chakra-ui/icons";
 
 const ProductPage = () => {
   const { id } = useParams();
@@ -23,25 +35,90 @@ const ProductPage = () => {
     queryKey: ["product", `${id}`],
     queryFn: () => getProduct(),
   });
-  console.log(data);
   useEffect(() => {
     document.title = `Products Store | Product ${data?.data?.attributes.title} page`;
-  }, [data?.data?.attributes.title]);
+  }, []);
   //        Render
   const RenderProduct = (): ReactNode => {
     const {
       id,
       attributes: { title, price, description, thumbnail },
     }: IProductResponse = data.data;
-    const props: IProduct = {
-      id,
-      title,
-      price,
-      description,
-      imageURL: thumbnail.data.attributes.formats.thumbnail.url,
-      buttonText: "ADD To Cart",
-    };
-    return <ProductCard key={id} attributes={props} />;
+
+    return (
+      <>
+        <Flex
+          maxW={"fit-content"}
+          maxH={"fit-content"}
+          justifyContent={"left"}
+          alignItems={"center"}
+          my={"2"}
+          onClick={() => navigate(-1)}
+          cursor={"pointer"}
+        >
+          <ArrowLeftIcon fontSize={"1.5rem"} mr={2} />
+          <Text maxH={"fit-content"} fontSize={"1.5rem"} fontWeight={"bold"}>
+            Back
+          </Text>
+        </Flex>
+        <Card
+          maxW="md"
+          key={id}
+          bg={"transparent"}
+          boxShadow={"xl"}
+          border={"1px solid #a8b5c8"}
+          mx={"auto"}
+        >
+          <CardBody>
+            <Image
+              src={`${import.meta.env.VITE_STRAPI_SERVER_URL}${
+                thumbnail.data.attributes.formats.thumbnail.url
+              }`}
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+              width={"350px"}
+              height={"350px"}
+              mx={"auto"}
+            />
+            <Stack mt="6" spacing="3">
+              <Heading size="lg" as={"h1"} mx={"auto"} color={"blue.600"}>
+                {title}
+              </Heading>
+              <Text
+                letterSpacing={"1.5px"}
+                lineHeight={"1.8"}
+                textAlign={"center"}
+                fontSize={"xl"}
+              >
+                {description}
+              </Text>
+              <Text
+                color="blue.600"
+                fontSize="3xl"
+                mx={"auto"}
+                fontWeight={"bold"}
+              >
+                ${price}
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter h={"auto"}>
+            <Button
+              variant="bold"
+              // colorScheme="blue"
+              fontSize={"2rem"}
+              bgColor={"#1da1f2"}
+              width={"100%"}
+              py={"2rem"}
+              _hover={{ backgroundColor: "#2b6cb0" }}
+            >
+              Add to cart
+            </Button>
+          </CardFooter>
+        </Card>
+      </>
+    );
   };
   const RenderProductSkeleton = <ProductSkeleton />;
   return (
